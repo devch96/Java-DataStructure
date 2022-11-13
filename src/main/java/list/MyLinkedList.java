b787797f06aae1e7162683d499a8347f22872a42
devch96 <chjung1006@gmail.com>
2022-11-13 19:40:09 +0900

    Ch02-04 리스트-Linked List(2)

--- src/main/java/list/MyLinkedList.java
+++ src/main/java/list/MyLinkedList.java
@@ -2,20 +2,55 @@ package list;
 
 public class MyLinkedList<T> implements IList<T>{
 
+    private int size;
+    private Node head;
+
+    public MyLinkedList(){
+        this.size=0;
+        this.head = new Node(null); // dummy head node
+    }
+    private class Node{
+        T data;
+        Node next;
+        Node(T data){
+            this.data = data;
+        }
+
+        Node(T data, Node next){
+            this.data = data;
+            this.next = next;
+        }
+    }
 
     @Override
     public void add(T t) {
-
+        Node curr = this.head;
+        while(curr.next != null){
+            curr = curr.next;
+        }
+        Node node = new Node(t);
+        curr.next = node;
+        this.size++;
     }
 
     @Override
     public void insert(int index, T t) {
-
+        Node prev = this.head;
+        Node curr = prev.next;
+        int i = 0;
+        while(i++<index){
+            prev = prev.next;
+            curr = curr.next;
+        }
+        Node node = new Node(t, curr);
+        prev.next=node;
+        this.size++;
     }
 
     @Override
     public void clear() {
-
+        this.size = 0;
+        this.head.next = null;
     }
 
     @Override
