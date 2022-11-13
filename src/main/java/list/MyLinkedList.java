dc87e1fbf4f7cd6a0d521617a562373ec4d77bac
devch96 <chjung1006@gmail.com>
2022-11-13 23:45:52 +0900

    ch02-05 리스트-Linked List

--- src/main/java/list/MyLinkedList.java
+++ src/main/java/list/MyLinkedList.java
@@ -35,6 +35,9 @@ public class MyLinkedList<T> implements IList<T>{
 
     @Override
     public void insert(int index, T t) {
+        if(index < 0 || index > this.size){
+            throw new IndexOutOfBoundsException();
+        }
         Node prev = this.head;
         Node curr = prev.next;
         int i = 0;
@@ -55,36 +58,86 @@ public class MyLinkedList<T> implements IList<T>{
 
     @Override
     public boolean delete(T t) {
+        Node prev = this.head;
+        Node current = prev.next;
+        while (current != null) {
+            if (current.data.equals(t)) {
+                prev.next = current.next;
+                current.next = null;
+                this.size--;
+                return true;
+            }
+            prev = prev.next;
+            current = current.next;
+        }
         return false;
     }
 
     @Override
     public boolean deleteByIndex(int index) {
-        return false;
+        if(index < 0 || index >= this.size){
+            throw new IndexOutOfBoundsException();
+        }
+        Node prev = this.head;
+        Node curr = prev.next;
+        int i  = 0;
+        while(i++<index){
+            prev = prev.next;
+            curr = curr.next;
+        }
+        prev.next = curr.next;
+        curr.next = null;
+        this.size--;
+        return true;
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if(index < 0 || index >= this.size){
+            throw new IndexOutOfBoundsException();
+        }
+        Node curr = this.head.next;
+        int i = 0;
+        while(i++<index){
+            curr = curr.next;
+        }
+        return curr.data;
     }
 
     @Override
     public int indexOf(T t) {
-        return 0;
+        Node curr = this.head.next;
+        int index = 0;
+        while(curr!=null){
+            if(curr.data.equals(t)){
+                return index;
+            }
+            curr = curr.next;
+            index++;
+
+        }
+        return -1;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return this.head.next==null;
     }
 
     @Override
     public boolean contains(T t) {
+        Node curr = this.head.next;
+        while(curr != null){
+            if(curr.data.equals(t) && curr.data != null){
+                return true;
+            }
+            curr = curr.next;
+        }
         return false;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 }
