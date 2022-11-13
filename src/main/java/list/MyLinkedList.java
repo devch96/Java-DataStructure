733a2d8084db5d281ce36effe0f5deef30b7d2f7
devch96 <chjung1006@gmail.com>
2022-11-13 19:24:01 +0900

    .gitignore updated

---
+++ src/main/java/list/MyLinkedList.java
@@ -0,0 +1,55 @@
+package list;
+
+public class MyLinkedList<T> implements IList<T>{
+
+
+    @Override
+    public void add(T t) {
+
+    }
+
+    @Override
+    public void insert(int index, T t) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean delete(T t) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteByIndex(int index) {
+        return false;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(T t) {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public boolean contains(T t) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
