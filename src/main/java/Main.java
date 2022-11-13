31fe8757d46640cdfd0e129f3a56a87ad29e18c4
devch96 <chjung1006@gmail.com>
2022-11-13 18:42:26 +0900

    ch02-01 리스트

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,15 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
+        list.add("World");
+        list.add("Grace");
+
+        System.out.println(list);
+
+        list.remove("Grace");
+        System.out.println(list);
+        System.out.println(list.get(0));
     }
 }
