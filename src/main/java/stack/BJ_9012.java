9265ed9e18b8d36a54dc0733d5bb9dc84298be95
devch96 <chjung1006@gmail.com>
2022-11-16 15:38:01 +0900

    ch03-03 스택을 이용한 알고리즘 문제

---
+++ src/main/java/stack/BJ_9012.java
@@ -0,0 +1,38 @@
+package stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class BJ_9012 {
+
+    public static void foo(String s){
+        Stack<Character> stack = new Stack<>();
+        int i = 0;
+        while(i++ < s.length()){
+            char c = s.charAt(i);
+            if(c == '('){
+                stack.push(c);
+            } else{
+                if(stack.size() <1){
+                    System.out.println("NO");
+                    return;
+                }
+                stack.pop();
+            }
+        }
+        if(stack.size()>0){
+            System.out.println("NO");
+        }else{
+            System.out.println("YES");
+        }
+    }
+    public static void main(String[] args) {
+        //input
+        Scanner scanner = new Scanner(System.in);
+        int T = scanner.nextInt();
+
+        for (int i = 0; i < T; i++) {
+            foo(scanner.next());
+        }
+    }
+}
