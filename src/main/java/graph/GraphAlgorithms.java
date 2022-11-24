9b0ba01ecab6727d88852a7f20247688539f6f25
devch96 <chjung1006@gmail.com>
2022-11-24 15:13:35 +0900

    ch09-07 위상정렬 구현

--- src/main/java/graph/GraphAlgorithms.java
+++ src/main/java/graph/GraphAlgorithms.java
@@ -45,4 +45,68 @@ public class GraphAlgorithms {
         }
         return result;
     }
+
+//      1. 모든 vertex 의 indegree 수를 센다
+//      2. 큐에 indegree 가 0인 vertex 삽입
+//      3. 큐에서 vertex 를 꺼내 연결된(나가는 방향) edge 제거
+//      4. 3번으로 인해 indegree 가 0 이 된 vertex 를 큐에 삽입
+//      5. 큐가 빌 때까지 3-4 반복
+
+    public static List<Integer> topologicalSortIndegree(IGraph graph){
+        //<vertex, indegree 갯수>
+        Map<Integer, Integer> indegreeCounter = graph.getIndegrees();
+        Queue<Integer> queue = new LinkedList<>();
+        List<Integer> result = new LinkedList<>();
+
+        for (int v : graph.getVertexes()) {
+            int count = indegreeCounter.getOrDefault(v, 0);
+            if(count == 0){
+                queue.offer(v);
+            }
+        }
+        while (!queue.isEmpty()) {
+            int node = queue.poll();
+            result.add(node);
+
+            for (int nn : graph.getNodes(node)) {
+                if(indegreeCounter.containsKey(nn)){
+                    int count = indegreeCounter.get(nn);
+                    if(count -1 == 0){
+                        queue.offer(nn);
+                    }
+                    indegreeCounter.put(nn, count - 1);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static List<Integer> topologicalSort(IGraph graph){
+        List<Integer> result = new ArrayList<>();
+        Stack<Integer> stack = new Stack<>();
+        Set<Integer> visited = new HashSet<>();
+
+        Set<Integer> vertexes = graph.getVertexes();
+        for(Integer vertex : vertexes){
+            if(!visited.contains(vertex)){
+                // dfs
+                topologicalSort(graph, vertex, visited, stack);
+            }
+        }
+        while(stack.size() > 0){
+            result.add(stack.pop());
+        }
+        return result;
+    }
+
+    private static void topologicalSort(IGraph graph, int vertex, Set<Integer> visited, Stack<Integer> stack){
+        visited.add(vertex);
+        List<Integer> nodes = graph.getNodes(vertex);
+        for(Integer n : nodes){
+            if(!visited.contains(n)){
+                topologicalSort(graph, n, visited, stack);
+            }
+        }
+        stack.push(vertex);
+    }
 }
