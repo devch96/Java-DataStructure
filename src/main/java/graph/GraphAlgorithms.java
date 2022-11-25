13b4d2c7290d81774d42199a78eda79519e8d951
devch96 <chjung1006@gmail.com>
2022-11-25 16:49:56 +0900

    ch09-09 다익스트라 구현

--- src/main/java/graph/GraphAlgorithms.java
+++ src/main/java/graph/GraphAlgorithms.java
@@ -109,4 +109,50 @@ public class GraphAlgorithms {
         }
         stack.push(vertex);
     }
+
+    /**
+    * 다익스트라 최단거리 알고리즘
+    * @param graph
+    * @param src 출발노드
+    * @param dst 도착노드
+    * @return 출발 노드로부터 도착 노드까지의 최단거리
+    */
+
+    public static int dijkstraShortestPath(IGraph graph, int src, int dst){
+        int size = 0;
+        for (int n : graph.getVertexes()) {
+            if(size<n){
+                size = n+1;
+            }
+        }
+
+        // distance 배열을 노드 크기만큼 초기화
+        int[] dist = new int[size];
+        for(int i =0 ; i < dist.length; i++){
+            dist[i] = Integer.MAX_VALUE; // distance 값을 INF 초기화
+        }
+        dist[src] = 0; // 시작노드의 distance 는 0
+        //<vertex, distance>
+        //distance를 기준으로하는 minheap
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            return a[1] - b[1];
+        });
+        pq.add(new int[]{src, 0});
+        while(!pq.isEmpty()){
+            int[] top = pq.poll();
+            int vertex = top[0];
+            int distance = top[1];
+
+            if(dist[vertex] < distance){
+                continue;
+            }
+            for(int node: graph.getNodes(vertex)){
+                if(dist[node] > dist[vertex] + graph.getDistance(vertex, node)){
+                    dist[node] = dist[vertex] + graph.getDistance(vertex, node);
+                    pq.add(new int[]{node, dist[node]});
+                }
+            }
+        }
+        return dist[dst];
+    }
 }
